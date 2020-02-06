02bb0095b286ad181f420b6c5c62c6ee084eaead
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2020-02-06 02:00:57 +0100

    fille interviewFeedback table out

--- data/migrations/20200206015426_interviewFeedbackTable.js
+++ data/migrations/20200206015426_interviewFeedbackTable.js
@@ -1,8 +1,21 @@
-
 exports.up = function(knex) {
-  
+  return knex.schema.createTable('interview_feedback', table => {
+    table.increments();
+    table.text('feedback', 5000).notNullable();
+    table.integer('rating').notNullable();
+    table
+      .integer('interview_id')
+      .unsigned()
+      .references('id')
+      .inTable('interview');
+      table
+      .integer('user_id')
+      .unsigned()
+      .references('id')
+      .inTable('user');
+  });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists('interview_feedback');
 };
