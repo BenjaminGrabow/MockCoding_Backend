2ef1c51b465b2cf804e61db911b29146e2a15a58
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2020-02-06 01:53:55 +0100

    delete unecessary cols

--- data/migrations/20200206013532_userTable.js
+++ data/migrations/20200206013532_userTable.js
@@ -3,15 +3,9 @@ exports.up = function(knex) {
     table.increments();
     table.string('first_name', 128).notNullable();
     table.string('last_name', 128).notNullable();
-    table
-      .string('email', 128)
-      .notNullable()
-      .unique();
+    table.string('email', 128).notNullable();
     table.string('password', 128).notNullable();
-    table.string('location');
-    table.string('avatar_url', 256).defaultTo('');
-    table.string('github', 128).defaultTo(null);
-    table.string('linkedin', 128).defaultTo(null);
+    table.string('avatar_url', 256).defaultTo(null);
   });
 };
 exports.down = function(knex) {
