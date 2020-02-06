5245cce33288a85b8b739343bf8443028f6837cd
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2020-02-06 01:59:35 +0100

    fill interview table out

--- data/migrations/20200206015414_interviewTable.js
+++ data/migrations/20200206015414_interviewTable.js
@@ -1,8 +1,30 @@
-
 exports.up = function(knex) {
-  
+  return knex.schema.createTable('interview', table => {
+    table.increments();
+    table.string('date', 100);
+    table.boolean('canceled').defaultTo(false);
+    table.boolean('finished').defaultTo(false);
+    table
+      .integer('user_id_one')
+      .notNullable()
+      .unsigned()
+      .references('id')
+      .inTable('user');
+    table
+      .integer('user_id_two')
+      .notNullable()
+      .unsigned()
+      .references('id')
+      .inTable('user');
+    table
+      .integer('topic_id')
+      .notNullable()
+      .unsigned()
+      .references('id')
+      .inTable('interview_topics');
+  });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists('interview');
 };
